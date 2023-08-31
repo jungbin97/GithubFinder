d192672ef86b50e6e6da76de678be80ea0e48e26
jungbin97 <ljb5054@gmail.com>
2023-08-31 22:07:48 +0900

    showProfile 작성 - 프로필 내용을 html로 렌더링

---
+++ script/ui.js
@@ -0,0 +1,48 @@
+class UI{
+    constructor(){
+        this.profile = document.querySelector('.profile');
+    }
+    
+    showProfile(user){
+        console.log("UI class");
+        
+        // 벡틱으로 
+        this.profile.innerHTML = `
+        <div class="card card-body mb-3">
+            <div class="row">
+                <div class="col-md-3">
+                    <img class="img-fluid mb-2" src="${user.avatar_url}">
+                    <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+                </div>
+                <div class="col-md-9">
+                    <div class="col-md-9">
+                        <span class="badge badge-primary">Public Repositories: ${user.public_repos}</span>
+                        <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span>
+                        <span class="badge badge-success">Followers: ${user.followers}</span>
+                        <span class="badge badge-info">Following: ${user.following}</span>
+                        <br><br>
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                Company: ${user.company}
+                            </li>
+                            <li class="list-group-item d-flex align-items-center">
+                                Blog: <a href="${user.blog}" target="_blank">${user.blog}</a>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                location: ${user.location}
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                Creaated_at: ${user.created_at}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <h3 class="page-heading mb-3">Latest Repositories</h3>
+            <div class="repos">
+            </div>
+        </div>
+        `;
+    }
+
+}
