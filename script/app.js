4ec22fb683aa0fbc55db1910ddac5399c939e5df
jungbin97 <ljb5054@gmail.com>
2023-09-01 11:55:25 +0900

    github-api 호출 한도 제한 - enter키를 누를 때만 서버에 요청하도록 횟수를 줄임

--- script/app.js
+++ script/app.js
@@ -3,24 +3,27 @@ const ui = new UI;
 
 const searchUser = document.querySelector(".searchUser");
 
-searchUser.addEventListener("keyup", (e) =>{
+searchUser.addEventListener("keydown", (e) =>{
     const userText = e.target.value;
     console.log(userText);
+    if(e.keyCode == 13){
+        e.preventDefault();
+        if(userText !== ''){
+            github.getUser(userText)
+            .then(data =>{
+                console.log(data);
+                if(data.profile.message === "Not Found"){
+                    //
+                } else{
+                    //profile HTML 렌더링
+                    ui.showProfile(data.profile);
+                    ui.showRepos(data.repos);
+                    //repos HTML 렌더링
+                }
+            })
+        }else{
+            //clear the profile
+    }
 
-    if(userText !== ''){
-        github.getUser(userText)
-        .then(data =>{
-            console.log(data);
-            if(data.profile.message === "Not Found"){
-                //
-            } else{
-                //profile HTML 렌더링
-                ui.showProfile(data.profile);
-                ui.showRepos(data.repos);
-                //repos HTML 렌더링
-            }
-        })
-    }else{
-        //clear the profile
     }
 })
